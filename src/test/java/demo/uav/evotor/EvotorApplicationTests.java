73717aadbdd3f318b5f4a95c146e518f3108a052
Andrey Ulyanov <ulyanov.andrey.vladimirovich@gmail.com>
2021-07-18 22:06:54 +0500

    Init project.

---
+++ src/test/java/demo/uav/evotor/EvotorApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.uav.evotor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvotorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
